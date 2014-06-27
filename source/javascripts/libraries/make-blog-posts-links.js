a9aea31bc91a8eac78e7cde57d52af602a9820ef
oncomouse <oncomouse@gmail.com>
2014-06-27 08:27:54 -0700

    Fixed for .blog members that don't have a data-url attribute.

--- source/javascripts/libraries/make-blog-posts-links.js
+++ source/javascripts/libraries/make-blog-posts-links.js
@@ -2,6 +2,9 @@ $(document).ready(function() {
 	$('.box.blog').each(function() {
 		var $this = $(this);
 		var url = $this.attr('data-url');
+		if (typeof url === "undefined") {
+			return;
+		}
 		$this.on("click", function(){
 			document.location.href = url;
 		});
