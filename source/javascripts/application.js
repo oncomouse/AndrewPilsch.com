220dd85abc1c4a091811414c51178d1d990d9c1d
oncomouse <oncomouse@gmail.com>
2014-08-16 14:45:52 -0700

    Switched to LAB.js now that yepnope is being discontinued. Also with no longer supporting IE8, no reason to use polyfills, anymore.

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,3 +1,5 @@
+//= require platform
+//= require ../../vendor/components/labjs/LAB.js
 //= require jquery
 //= require eventEmitter
 //= require eventie
@@ -5,3 +7,5 @@
 //= require vendor/jquery/plugins/jquery.isotope.min.js
 //= require plugins.js
 //= require_tree ./libraries
+
+if(platform.name == "IE" && parseInt(platform.version) < 9) { window.location.href="ie8.html"; }
