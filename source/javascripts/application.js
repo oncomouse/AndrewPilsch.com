bca2ae0d570b222bb2c32ef45dfcff71326157d4
oncomouse <oncomouse@gmail.com>
2014-08-18 13:39:25 -0700

    All main site javascript is now managed through Bower.

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,10 +1,24 @@
-//= require platform
+//=	 require platform
 //= require ../../vendor/components/labjs/LAB.js
 //= require jquery
 //= require eventEmitter
 //= require eventie
 //= require imagesloaded
-//= require vendor/jquery/plugins/jquery.isotope.min.js
+//= require doc-ready
+//= require get-style-property
+//= require get-size
+//= require jquery-bridget
+//= require matches-selector
+//= require ../../vendor/components/outlayer/item.js
+//= require ../../vendor/components/outlayer/outlayer.js
+//= require masonry
+//= require ../../vendor/components/isotope/js/item.js
+//= require ../../vendor/components/isotope/js/layout-mode.js
+//= require ../../vendor/components/isotope/js/isotope.js
+//= require ../../vendor/components/isotope/js/layout-modes/vertical.js
+//= require ../../vendor/components/isotope/js/layout-modes/fit-rows.js
+//= require ../../vendor/components/isotope/js/layout-modes/masonry.js
+// require vendor/jquery/plugins/jquery.isotope.min.js
 //= require plugins.js
 //= require_tree ./libraries
 
