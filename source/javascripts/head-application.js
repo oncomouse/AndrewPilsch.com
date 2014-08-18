4faaf3b6f54feb3b9ac1f32b7172c3b2e4e23762
oncomouse <oncomouse@gmail.com>
2014-08-18 13:40:08 -0700

    Non-local scripts removed.

--- source/javascripts/head-application.js
+++
@@ -1,5 +0,0 @@
-//= require platform
-//= require vendor/modernizr/modernizr.min.js
-// Redirect if IE 8
-//var v=-1;if("Microsoft Internet Explorer"==navigator.appName){var r=/MSIE ([0-9]{1,}[.0-9]{0,})/;null!=r.exec(navigator.userAgent)&&(v=parseFloat(RegExp.$1))}-1<v&&8>=v&&(window.location.href="ie8.html");
-if(platform.name == "IE" && parseInt(platform.version) < 9) { window.location.href="ie8.html"; }
