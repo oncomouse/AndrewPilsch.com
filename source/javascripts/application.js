cde0691165f90db2dd2c94f29c96f658d643bfc8
onocmouse <onocmouse@gmail.com>
2015-07-29 13:04:50 -0500

    Shifted the init code to application.js (except for the asset paths).

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -7,3 +7,21 @@
 //= require_tree ./libraries
 
 if(platform.name == 'IE' && parseInt(platform.version) < 9) { window.location.href='ie8.html'; }
+
+// Load polyfills and init isotope. All file paths are generated at build by Middleman:
+$(document).ready(function() {
+
+	if (platform.os.family != "OS X") {
+		$('<link rel="stylesheet" type="text/css" href="' + window.assets.cssBaseUrl + 'fonts/liberation-sans/liberation-sans.css" >') .appendTo("head");
+	}
+	if(window.matchMedia && window.matchMedia("all and (min-width: 33.236em)").matches) {
+		$LAB.queueScript(window.assets.baseUrl + 'site.js');
+	} else {
+		$LAB.queueScript(window.assets.baseUrl + 'mobile.js');
+	}
+
+	$LAB.queueWait(function() {
+		start_isotope();
+	});
+	$LAB.runQueue();
+});
