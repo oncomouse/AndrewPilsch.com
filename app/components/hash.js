6c395f24379f281a28b77f0bd4a0b62627657698
onocmouse <onocmouse@gmail.com>
2017-05-05 10:28:46 -0500

    A potentially over-engineered solution to internal box hyperlinking, but it is consistent with the Redux philosophy in which components dispatch to the store and the store feeds changes to subscribers.

--- app/components/hash.js
+++ app/components/hash.js
@@ -18,4 +18,13 @@ HashManager.prototype.onReady = function() {
 	if(window.location.hash !== '' && window.location.hash !== '#') {
 		this.store.dispatch(openBox(window.location.hash));
 	}
+	// This code tells all box links that have a hash that is also a box ID (internal hyperlinking to other boxes) to dispatch the proper Redux event when clicked. This can also be handled (with less scripting) by a hashchange event, but that feels like it decenters the Redux-y-ness of the project. I think this is the right way to do this because it only involves changing the hash manager. If I were to trigger off a hashchange event, the way boxes get opened would not use Redux. It would instead just change the hash. This feels like the kind of event chaining that this project was designed to prevent:
+	document.querySelectorAll('.box a[href^="#"]').forEach((el) => {
+		if(el.hash !== '' && document.querySelector(el.hash) !== null) {
+			el.addEventListener('click', (ev) => {
+				ev.preventDefault();
+				this.store.dispatch(openBox(el.hash));
+			});
+		}
+	});
 }
