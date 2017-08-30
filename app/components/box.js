511237ed763df9ca1c69697402fb9c19090e683f
oncomouse <oncomouse@gmail.com>
2017-08-30 15:02:18 -0500

    Converted to ES6 class syntax

--- app/components/box.js
+++ app/components/box.js
@@ -22,79 +22,81 @@ function whichTransitionEvent(){
     }
 }
 
-export default function BoxManager(store, isotope) {
-	this.store = store;
-	this.isotope = isotope;
-	this.openBox = getOpenBox(this.store);
-	this.store.subscribe(this.handleBoxStateChange.bind(this));
-	this.transitionEvent = whichTransitionEvent();
-}
-BoxManager.prototype.dispatchOpenBox = function(event) {
-	event.preventDefault();
-	this.store.dispatch(openBox(`#${event.currentTarget.id}`));
-}
-BoxManager.prototype.dispatchCloseBox = function(event) {
-	event.preventDefault();
-	this.store.dispatch(closeBox());
-}
-BoxManager.prototype.renderOpenBox = function(elem) {
-	elem.classList.add('expanded');
-	this.transitionEvent & elem.addEventListener(this.transitionEvent, (e) => {
-		// Scroll to the open box on arrangeComplete:
-		this.isotope.once('arrangeComplete', () => scrollIt(offset(e.target).top - (this.columnPadding * 2), 500));
-		// Trigger rearrangement:
-		this.isotope.arrange();
-	}, {once: true});
-}
-BoxManager.prototype.renderCloseBox = function(elem) {
-	if(elem === null) { return }
-	// Box sizing is now handled via CSS:
-	elem.classList.remove('expanded');
-	this.transitionEvent & elem.addEventListener(this.transitionEvent, (e) => {
-		// Trigger rearrangment:
-		this.isotope.arrange();
-		// Reattach the ability to open the box:
-		e.target.addEventListener('click', this.dispatchOpenBox.bind(this), {once: true});
-	}, {once: true});
-}
-BoxManager.prototype.handleBoxStateChange = function() {
-	const openBox = getOpenBox(this.store);
-	if(openBox !== this.openBox) {
-		// Restore the open box:
-		this.renderCloseBox(document.querySelector('.expanded'));
-		// We are opening a new box:
-		if(openBox !== null) {
-			// Set box size to expanded sizes:
-			this.renderOpenBox(document.querySelector(openBox));
-			// Append "Close All" Button (if missing):
-			if(document.querySelector('#close_all') === null) {
-				// Insert the button:
-				document.querySelector('#filters ul').insertAdjacentHTML('beforeend', '<li id="close_all"><a href="">Close All</a></li>');
-				// Add the event listener:
-				document.querySelector('#close_all a').addEventListener('click', this.dispatchCloseBox.bind(this));
-			}
-		} else {
-			// Remove "Close All" Button:
-			document.querySelector('#close_all').parentNode.removeChild(document.querySelector('#close_all'));
-		}
-		this.openBox = openBox;
-		this.isotope.arrange();
+export default class BoxManager {
+	constructor(store, isotope) {
+		this.store = store;
+		this.isotope = isotope;
+		this.openBox = getOpenBox(this.store);
+		this.store.subscribe(this.handleBoxStateChange.bind(this));
+		this.transitionEvent = whichTransitionEvent();
 	}
-}
-BoxManager.prototype.loadImages = function () {
-	this.store.dispatch(loadImages())
-}
-BoxManager.prototype.onReady = function() {
-	// Set up:
-	this.defaultSize = [width(document.querySelector('#large_box_size')), height(document.querySelector('#large_box_size'))];
-	this.columnPadding = width(document.querySelector('#column_padding'));
+	dispatchOpenBox(event) {
+		event.preventDefault();
+		this.store.dispatch(openBox(`#${event.currentTarget.id}`));
+	}
+	dispatchCloseBox(event) {
+		event.preventDefault();
+		this.store.dispatch(closeBox());
+	}
+	renderOpenBox(elem) {
+		elem.classList.add('expanded');
+		this.transitionEvent & elem.addEventListener(this.transitionEvent, (e) => {
+			// Scroll to the open box on arrangeComplete:
+			this.isotope.once('arrangeComplete', () => scrollIt(offset(e.target).top - (this.columnPadding * 2), 500));
+			// Trigger rearrangement:
+			this.isotope.arrange();
+		}, {once: true});
+	}
+	renderCloseBox(elem) {
+		if(elem === null) { return }
+		// Box sizing is now handled via CSS:
+		elem.classList.remove('expanded');
+		this.transitionEvent & elem.addEventListener(this.transitionEvent, (e) => {
+			// Trigger rearrangment:
+			this.isotope.arrange();
+			// Reattach the ability to open the box:
+			e.target.addEventListener('click', this.dispatchOpenBox.bind(this), {once: true});
+		}, {once: true});
+	}
+	handleBoxStateChange() {
+		const openBox = getOpenBox(this.store);
+		if(openBox !== this.openBox) {
+			// Restore the open box:
+			this.renderCloseBox(document.querySelector('.expanded'));
+			// We are opening a new box:
+			if(openBox !== null) {
+				// Set box size to expanded sizes:
+				this.renderOpenBox(document.querySelector(openBox));
+				// Append "Close All" Button (if missing):
+				if(document.querySelector('#close_all') === null) {
+					// Insert the button:
+					document.querySelector('#filters ul').insertAdjacentHTML('beforeend', '<li id="close_all"><a href="">Close All</a></li>');
+					// Add the event listener:
+					document.querySelector('#close_all a').addEventListener('click', this.dispatchCloseBox.bind(this));
+				}
+			} else {
+				// Remove "Close All" Button:
+				document.querySelector('#close_all').parentNode.removeChild(document.querySelector('#close_all'));
+			}
+			this.openBox = openBox;
+			this.isotope.arrange();
+		}
+	}
+	loadImages () {
+		this.store.dispatch(loadImages())
+	}
+	onReady() {
+		// Set up:
+		this.defaultSize = [width(document.querySelector('#large_box_size')), height(document.querySelector('#large_box_size'))];
+		this.columnPadding = width(document.querySelector('#column_padding'));
 	
-	// Box Expansion Events:
-	document.querySelectorAll('.box.expand').forEach((elem) => {
-		elem.addEventListener('click', this.dispatchOpenBox.bind(this), {once: true});
-	});
-	this.isotope.once('layoutComplete', this.loadImages.bind(this));
-	// Trigger Masonry Layout:
-	this.isotope.layout();
+		// Box Expansion Events:
+		document.querySelectorAll('.box.expand').forEach((elem) => {
+			elem.addEventListener('click', this.dispatchOpenBox.bind(this), {once: true});
+		});
+		this.isotope.once('layoutComplete', this.loadImages.bind(this));
+		// Trigger Masonry Layout:
+		this.isotope.layout();
+	}
 }
 
--- app/components/filter.js
+++ app/components/filter.js
@@ -1,36 +1,38 @@
 import {getCurrentFilter} from 'store';
 import {changeFilter} from 'actions/filterActions';
 
-export default function FilterManager(store, isotope) {
-	this.store = store;
-	this.isotope = isotope;
-	this.filterState = getCurrentFilter(this.store);
-	this.store.subscribe(this.updateFilter.bind(this));
-}
-FilterManager.prototype.updateFilter = function() {
-	let currentFilter = getCurrentFilter(this.store);
-	if(this.filterState !== currentFilter){
-		this.isotope.arrange({
-			filter: currentFilter
-		});
-		this.filterState = currentFilter;
-		document.querySelector('#filters a.selected').classList.remove('selected');
-		document.querySelector(`#filters a[data-filter="${currentFilter === null ? "" : currentFilter}"]`).classList.add('selected');
+export default class FilterManager {
+	constructor(store, isotope) {
+		this.store = store;
+		this.isotope = isotope;
+		this.filterState = getCurrentFilter(this.store);
+		this.store.subscribe(this.updateFilter.bind(this));
 	}
-}
-FilterManager.prototype.changeFilter = function(ev) {
-	ev.preventDefault();
-	
-	let filterTarget = ev.target.getAttribute('data-filter');
-	if(filterTarget === '') {
-		filterTarget = null;
+	updateFilter() {
+		let currentFilter = getCurrentFilter(this.store);
+		if(this.filterState !== currentFilter){
+			this.isotope.arrange({
+				filter: currentFilter
+			});
+			this.filterState = currentFilter;
+			document.querySelector('#filters a.selected').classList.remove('selected');
+			document.querySelector(`#filters a[data-filter="${currentFilter === null ? "" : currentFilter}"]`).classList.add('selected');
+		}
 	}
-	this.store.dispatch(changeFilter(filterTarget))
-}
-FilterManager.prototype.onReady = function() {
-	// All is the default filter state:
-	document.querySelector('#filters a[data-filter=""]').classList.add('selected');
+	changeFilter(ev) {
+		ev.preventDefault();
 	
-	// Attach filter click events:
-	document.querySelectorAll('#filters a').forEach((el) => el.addEventListener('click', this.changeFilter.bind(this)));
+		let filterTarget = ev.target.getAttribute('data-filter');
+		if(filterTarget === '') {
+			filterTarget = null;
+		}
+		this.store.dispatch(changeFilter(filterTarget))
+	}
+	onReady() {
+		// All is the default filter state:
+		document.querySelector('#filters a[data-filter=""]').classList.add('selected');
+	
+		// Attach filter click events:
+		document.querySelectorAll('#filters a').forEach((el) => el.addEventListener('click', this.changeFilter.bind(this)));
+	}
 }
