ea43aa2fddffacadd1dbe3f7648d3038013d5562
oncomouse <oncomouse@gmail.com>
2017-09-07 15:04:57 -0500

    Slotted in (but haven't deployed) using mobile-detect.js so that front-end and back-end detection are the same. I don't like this approach.

--- app/index.js
+++ app/index.js
@@ -1,3 +1,6 @@
+//import MobileDetect from 'mobile-detect'
+//const md = new MobileDetect(window.navigator.userAgent);
+//
 if(window.matchMedia && window.matchMedia("all and (min-width: 33.236em)").matches) {
 	System.import('site').then(module => module.default())
 } else {
