8ae87e83032486dc89c26df336b64502f103287a
oncomouse <oncomouse@gmail.com>
2017-10-01 10:03:50 -0500

    No longer use isotope for mobile, so no need to configure it

--- app/isotope/configureIsotope.js
+++ app/isotope/configureIsotope.js
@@ -1,24 +1,11 @@
 import Isotope from 'isotope-layout';
 
-export default (mobile=false) => {
-	return mobile ? 
-		new Isotope(document.querySelector('#box_container'), {
-			itemSelector: '.box',
-			isInitLayout: false,
-			layoutMode : 'vertical',
-			animationOptions: {
-				duration: 200,
-				easing: 'linear',
-				queue: false
-			}
-		}) :
-		new Isotope(document.querySelector('#box_container'), {
-			itemSelector: '.box',
-			isInitLayout: false,
-			layoutMode : 'masonry',
-			masonry: {
-				columnWidth: parseInt(window.getComputedStyle(document.querySelector('#column_width')).width)
-			},
-			cornerStampSelector: '.corner_stamp'
-		});
-}
+export default (mobile=false) => new Isotope(document.querySelector('#box_container'), {
+	itemSelector: '.box',
+	isInitLayout: false,
+	layoutMode : 'masonry',
+	masonry: {
+		columnWidth: parseInt(window.getComputedStyle(document.querySelector('#column_width')).width)
+	},
+	cornerStampSelector: '.corner_stamp'
+});
