699af634d84f15854517f015c9c3a9c7ea76d2aa
oncomouse <oncomouse@gmail.com>
2020-06-29 15:04:43 -0500

    Use version info for packed asset file names.

--- _scripts/embed-js.js
+++ _scripts/embed-js.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
-const { JSDOM } = require('jsdom');
+const {execSync} = require('child_process');
+const {JSDOM} = require('jsdom');
 const argv = require('minimist')(process.argv.slice(2), {
   boolean: [
     'inline',
@@ -10,7 +11,7 @@ const argv = require('minimist')(process.argv.slice(2), {
   },
 });
 // Use with --inline to replace inclusions with their file source
-
+const gitRevision = execSync('git rev-parse HEAD').toString().replace(/\n/g, '');
 const processInclude = (el, type, files) => {
   const src = el.getAttribute(type === 'css' ? 'href' : 'src');
   if (src.indexOf('cdnjs.cloudflare.com') >= 0) return;
@@ -21,7 +22,7 @@ const processInclude = (el, type, files) => {
     const fileSrc = fs.readFileSync(path.join(...src.replace(/^http[s]{0,1}\:\/\/[^/]+\//, './_site/').split('/'))).toString();
     files.addFile(src, fileSrc);
   }
-  if(argv.inline) {
+  if (argv.inline) {
     // Write the file as an inline inclusion:
     el.outerHTML = (type === 'css' ? `<style>${files.getFile(src)}</style>` : `<script>${files.getFile(src)}</script>`);
   } else {
@@ -30,23 +31,23 @@ const processInclude = (el, type, files) => {
   }
 }
 
-const Files = function() { this.files = {}; };
-Files.prototype.addFile = function(src, fileSrc) { this.files[src] = fileSrc; };
-Files.prototype.has = function(src) { return Object.prototype.hasOwnProperty.call(this.files, src); }
-Files.prototype.getFile = function(src) {
+const Files = function () {this.files = {};};
+Files.prototype.addFile = function (src, fileSrc) {this.files[src] = fileSrc;};
+Files.prototype.has = function (src) {return Object.prototype.hasOwnProperty.call(this.files, src);}
+Files.prototype.getFile = function (src) {
   return this.files[src];
 }
-Files.prototype.getFiles = function(filter=undefined) {
-  if(filter === undefined) return Object.values(this.files);
+Files.prototype.getFiles = function (filter = undefined) {
+  if (filter === undefined) return Object.values(this.files);
   return this.getNames(filter)
     .reduce((acc, cur) => ([
       ...acc,
       this.files[cur],
     ]), []);
 }
-Files.prototype.getNames = function(filter=undefined) {
+Files.prototype.getNames = function (filter = undefined) {
   const keys = Object.keys(this.files);
-  if(filter === undefined) return keys;
+  if (filter === undefined) return keys;
   return Object.keys(this.files)
     .filter(path => path.match(filter));
 }
@@ -67,12 +68,12 @@ argv._.forEach((file) => {
   if (!argv.inline) {
     // If the site had CSS assets, we write the packed CSS file:
     if (seenCss) {
-      const cssPath = files.getNames(/site\.css$/)[0].replace('site', 'packed');
+      const cssPath = files.getNames(/site\.css$/)[0].replace('site', `packed-${gitRevision}`);
       document.querySelector('head').insertAdjacentHTML('beforeend', `<link rel="stylesheet" href="${cssPath}" />`);
     }
     // If the site had JS assets, we write the packed JS file:
     if (seenJs) {
-      const jsPath = files.getNames(/site\.js$/)[0].replace('site', 'packed');
+      const jsPath = files.getNames(/site\.js$/)[0].replace('site', `packed-${gitRevision}`);
       document.querySelector('body').insertAdjacentHTML('beforeend', `<script src="${jsPath}" />`);
     }
   }
@@ -81,12 +82,12 @@ argv._.forEach((file) => {
 if (!argv.inline) {
   // Write the packed CSS file:
   fs.writeFileSync(
-    path.join('_site', 'css', 'packed.css'),
+    path.join('_site', 'css', `packed-${gitRevision}.css`),
     files.getFiles(/\.css$/).join(''),
   );
   // Write the packed JS file:
   fs.writeFileSync(
-    path.join('_site', 'js', 'packed.js'),
+    path.join('_site', 'js', `packed-${gitRevision}.js`),
     files.getFiles(/\.js$/).join(''),
   );
 }
