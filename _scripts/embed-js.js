d9f704fe6a76090d41756ce620bb88c05cfee8f3
oncomouse <oncomouse@gmail.com>
2020-07-16 08:57:49 -0500

    Universal Asset packing script

--- _scripts/embed-js.js
+++ _scripts/embed-js.js
@@ -1,20 +1,25 @@
 const fs = require('fs');
+const yaml = require('js-yaml');
 const path = require('path');
 const {execSync} = require('child_process');
 const {JSDOM} = require('jsdom');
 const argv = require('minimist')(process.argv.slice(2), {
   boolean: [
     'inline',
+    'delete',
   ],
   default: {
     'inline': false,
+    'delete': false,
   },
 });
 const disallowedList = [
   'imagesloaded',
 ];
+const jekyllConfig = yaml.safeLoad(fs.readFileSync('./_config.yml'))
 const checkDisallowedList = src => disallowedList.reduce((acc, cur) => acc || src.indexOf(cur) >= 0, false);
 // Use with --inline to replace inclusions with their file source
+const has = (obj, key) => Object.prototype.hasOwnProperty.call(obj, key)
 const gitRevision = execSync('git rev-parse HEAD').toString().replace(/\n/g, '');
 const processInclude = (el, type, files) => {
   const src = el.getAttribute(type === 'css' ? 'href' : 'src');
@@ -29,7 +34,18 @@ const processInclude = (el, type, files) => {
     return;
   }
   if (!files.has(src)) {
-    const fileSrc = fs.readFileSync(path.join(...src.replace(/^http[s]{0,1}\:\/\/[^/]+\//, './_site/').split('/'))).toString();
+    let filePath = src
+    if (has(jekyllConfig, 'url') && jekyllConfig.url.length > 0) {
+      filePath = filePath.replace(jekyllConfig.url, '')
+    }
+    if (has(jekyllConfig, 'baseurl') && jekyllConfig.baseurl.length > 0) {
+      filePath = filePath.replace(jekyllConfig.baseurl, '')
+    }
+    filePath = path.join('_site', filePath)
+    const fileSrc = fs.readFileSync(filePath).toString();
+    if (fs.existsSync(filePath) && argv.delete) {
+      fs.unlinkSync(filePath)
+    }
     files.addFile(src, fileSrc);
   }
   if (argv.inline) {
@@ -71,19 +87,20 @@ argv._.forEach((file) => {
     processInclude(el, 'css', files);
     return true;
   }, false);
-  const seenJs = Array.from(document.querySelectorAll('script[src]')).reduce((seen, el) => {
+  const seenJs = Array.from(document.querySelectorAll('script[src]')).reduce((_seen, el) => {
     processInclude(el, 'js', files);
     return true;
   }, false);
+  const absoluteUrl = file => `${has(jekyllConfig, 'url') ? jekyllConfig.url : ''}${has(jekyllConfig, 'baseurl') ? jekyllConfig.baseurl : ''}/${file}`
   if (!argv.inline) {
     // If the site had CSS assets, we write the packed CSS file:
     if (seenCss) {
-      const cssPath = files.getNames(/site\.css$/)[0].replace('site', `packed-${gitRevision}`);
+      const cssPath = absoluteUrl(`packed-${gitRevision}.css`);
       document.querySelector('head').insertAdjacentHTML('beforeend', `<link rel="stylesheet" href="${cssPath}" />`);
     }
     // If the site had JS assets, we write the packed JS file:
     if (seenJs) {
-      const jsPath = files.getNames(/site\.js$/)[0].replace('site', `packed-${gitRevision}`);
+      const jsPath = absoluteUrl(`packed-${gitRevision}.js`);
       document.querySelector('body').insertAdjacentHTML('beforeend', `<script src="${jsPath}" />`);
     }
   }
@@ -92,12 +109,12 @@ argv._.forEach((file) => {
 if (!argv.inline) {
   // Write the packed CSS file:
   fs.writeFileSync(
-    path.join('_site', 'css', `packed-${gitRevision}.css`),
+    path.join('_site', `packed-${gitRevision}.css`),
     files.getFiles(/\.css$/).join(''),
   );
   // Write the packed JS file:
   fs.writeFileSync(
-    path.join('_site', 'js', `packed-${gitRevision}.js`),
+    path.join('_site', `packed-${gitRevision}.js`),
     files.getFiles(/\.js$/).join(''),
   );
 }
